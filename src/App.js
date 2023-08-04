3c0a8a48ed8e73830ef64368d64390294c0fcf54
heltnx <helenetnx@yahoo.fr>
2023-08-04 20:45:05 +0200

    agencement fichiers et debut router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Nav from './components/Nav.js';
+import './assets/style/App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="../images/logo.svg" className="App-logo" alt="logo" />
+        <Nav />
       </header>
+      <div className="App-banniere"><h1>Chez vous, partout et ailleurs</h1></div>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <ul>
+      <li><a href="/acceuil">Accueil</a></li>
+      <li><a href="/a propos">À Propos</a></li>
+    </ul>
+  );
+}
+
+export default Nav;
