944918b9b493ae6f8755e3ca335c9899a63f623c
heltnx <helenetnx@yahoo.fr>
2023-08-11 17:09:20 +0200

    reglages

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,19 +1,15 @@
 // Logement.js
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
 // css
-import '../style/banniere.css';
 import '../style/logement.css';
 
 const Logement = () => {
   const { id } = useParams();
-
   return (
     <div>
       <div className='logement'>
         <h2>Logement n° {id}</h2>
-        {/* Afficher les détails du logement ici */}
       </div>
     </div>
   );
